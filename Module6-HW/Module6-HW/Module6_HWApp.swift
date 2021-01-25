b8770e316b31801a47477aedbaf31effb0165387
MaksimDemin <mdeminalex@icloud.com>
2021-01-25 15:53:49 +0200

    Create new project HW Module 6

---
+++ Module6-HW/Module6-HW/Module6_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module6_HWApp.swift
+//  Module6-HW
+//
+//  Created by Maksim Demin on 25.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Module6_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
